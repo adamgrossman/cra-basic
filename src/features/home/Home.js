65fca18b44f2d8313ccd03404cb900977a8ad7b4
Adam <adam.grossman08@me.com>
2021-02-04 08:54:41 -0800

    Add fake axios call

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { useSelector } from "react-redux";
 import "./Home.css";
 
 const Home = () => {
+  const [fakeData, setFakeData] = useState();
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
+      const persons = res.data;
+      setFakeData(persons);
+    });
+  }, []);
+
+  console.log("fakeData :>> ", fakeData);
+
   return (
     <div className="container">
       <h1>Hi there!</h1>
